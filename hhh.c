b17cf7728cb1222a814a54e6343cc965f2b0dd4a
kiruthikav14497 <kiruthika.vece@ksrce.ac.in>
2017-06-19 15:38:08 +0530

    Add files via upload

---
+++ hhh.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int n,i;
+printf("enter the value of");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+    factorial*=i;
+}
+printf("the value of n is");
+scanf("%d",&n);
+}
